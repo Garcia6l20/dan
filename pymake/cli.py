33cf7098fc1ce2ba27c99f4048706f5737a58b47
Sylvain Garcia <garcia.6l20@gmail.com>
2022-12-06 10:45:20 +0100

    cli common options

--- pymake/cli.py
+++ pymake/cli.py
@@ -10,30 +10,67 @@ logging.basicConfig(level=logging.INFO)
 
 pass_make = click.make_pass_decorator(Make)
 
+_active_targets_initialized = False
+def _set_targets(ctx, param, value):
+    if len(value) == 0:
+        return
+
+    global _active_targets_initialized
+    if not _active_targets_initialized:
+        _active_targets_initialized = True
+        ctx.obj.active_targets = dict()
+
+    found_targets = {name: target for name, target in ctx.obj.all_targets.items() if name.endswith(value)}
+
+    if len(found_targets) == 0:
+        raise RuntimeError(f"cannot math any target for name '{value}'")
+
+    ctx.obj.active_targets.update(found_targets)
+
+_common_opts = [
+    click.argument('TARGETS', nargs=-1, callback=_set_targets)
+]
+
+def add_options(options):
+    def _add_options(func):
+        for option in reversed(options):
+            func = option(func)
+        return func
+    return _add_options
 
 @click.group(invoke_without_command=True)
 @click.option('--debug', '-d', is_flag=True, help='Pring debug informations')
-@click.option('--target', '-t', help='Target to build', multiple=True)
+# @click.option('--target', '-t', help='Target to build', multiple=True)
 @click.pass_context
-def cli(ctx: click.Context, debug: bool, target: list[str]):
+def cli(ctx: click.Context, debug: bool):
     logging.getLogger().setLevel(logging.DEBUG if debug else logging.INFO)
-    ctx.obj = Make(active_targets=target)
+    ctx.obj = Make()
     if ctx.invoked_subcommand is None:
         ctx.invoke(build)
 
 
 @cli.command()
+@add_options(_common_opts)
 @pass_make
-def build(make: Make):
+def build(make: Make, **kwargs):
     asyncio.run(make.build())
 
 
 @cli.command()
+@add_options(_common_opts)
 @pass_make
-def clean(make: Make):
+def list(make: Make, **kwargs):
+    for name, target in make.active_targets.items():
+        click.echo(f'{name} - {type(target).__name__}')
+
+@cli.command()
+@add_options(_common_opts)
+@pass_make
+def clean(make: Make, **kwargs):
     asyncio.run(make.clean())
 
 @cli.command()
+@add_options(_common_opts)
 @pass_make
-def run(make: Make):
+def run(make: Make, **kwargs):
     asyncio.run(make.run())
