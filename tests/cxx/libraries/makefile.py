a35a24d005d874ed622ad36ccbf6989e3329eda5
Sylvain Garcia <sylvain.garcia@non.se.com>
2022-12-07 00:46:54 +0100

    add c++17 flag

--- tests/cxx/libraries/makefile.py
+++ tests/cxx/libraries/makefile.py
@@ -4,9 +4,11 @@ from pymake import cli
 from pymake.cxx import Library, Executable
 from copy import deepcopy
 
-static = Library(sources=['static.cpp'], public_includes=['.'])
+compile_options = {'-std=c++17'}
+
+static = Library(sources=['static.cpp'], public_includes=['.'], public_compile_options=compile_options)
 statically_linked = Executable(sources=['main.cpp'], dependencies=[static])
-shared = Library(sources=['shared.cpp'], public_includes=['.'], static=False)
+shared = Library(sources=['shared.cpp'], public_includes=['.'], static=False, public_compile_options=compile_options)
 shared_linked = Executable(sources=['main.cpp'], dependencies=[shared])
 
 if __name__ == '__main__':
