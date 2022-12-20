aa0aaecfaafac8e9d1960afd930bb4472c562701
Sylvain Garcia <garcia.6l20@gmail.com>
2022-12-20 14:09:59 +0100

    add tar source

--- pymake/core/target.py
+++ pymake/core/target.py
@@ -194,6 +194,7 @@ class Target(Logging):
         self.debug('preloading...')
         await asyncio.gather(*[obj.preload() for obj in self.target_dependencies])
         await asyncio.gather(*[obj.initialize() for obj in self.preload_dependencies])
+        await asyncio.gather(*[obj.build() for obj in self.preload_dependencies])
 
     @asyncio.once_method
     async def initialize(self):
---
+++ pymake/smc/tar.py
@@ -0,0 +1,42 @@
+from click import Path
+from pymake.core import asyncio, aiofiles
+from pymake.core.target import Target
+from pymake.core.utils import AsyncRunner, chdir
+from pymake.logging import Logging
+import aiohttp
+import tarfile
+
+async def fetch_file(url, dest):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            assert resp.status == 200
+            data = await resp.read()
+
+    async with aiofiles.open(
+        dest, "wb"
+    ) as outfile:
+        await outfile.write(data)
+        
+class TarSources(Target, Logging, AsyncRunner):
+    def __init__(self, name: str, url: str, version: str = None) -> None:
+        super().__init__(name, version=version, all=False)
+        self.url = url
+        self.archive_name = url.split("/")[-1]
+        self.output: Path = self.build_path / 'sources'
+
+    @asyncio.once_method
+    async def initialize(self):
+        await self.preload()
+
+        if not self.clean_request:
+            if not self.output.exists():
+                self.info(f'downloading {self.url}')
+                await fetch_file(self.url, self.build_path / self.archive_name)
+                with tarfile.open(self.build_path / self.archive_name) as f:
+                    self.info(f'extracting {self.archive_name}')
+                    f.extractall(self.output)
+
+        return await super().initialize(recursive_once=True)
+
+    async def __call__(self):
+        pass
