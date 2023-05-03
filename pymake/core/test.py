56cb303a204336ef6369bbe704e5dae63fddfc4b
Sylvain Garcia <garcia.6l20@gmail.com>
2023-05-03 06:56:47 +0000

    fix test basename

--- pymake/core/test.py
+++ pymake/core/test.py
@@ -19,34 +19,30 @@ class Test(Logging):
     name: str = None
     fullname: str = None
 
-    cases: t.Iterable[tuple[t.Iterable[t.Any], int]]  = [((), 0)]
+    cases: t.Iterable[tuple[t.Iterable[t.Any], int]] = [((), 0)]
     """Test cases
     
     list of args giving a return value
     """
 
     executable: type[AsyncExecutable] = None
-    
+
     file: Path | str = None
     lineno: int = None
 
     makefile: None
-    
+
     def __init__(self, *args, **kwargs):
         # in case of inheritance usage, we must initialize the AsyncExecutable part
         super().__init__(*args, **kwargs)
-        
+
         if self.name is None:
             self.name = self.__class__.__name__
-        
-        # in case of inheritance usage, makefile may already exists 
-        if not hasattr(self, 'makefile'):
-            from pymake.core.include import context
-            self.makefile = context.current
 
         if not self.executable:
             if not hasattr(self, 'execute'):
-                raise RuntimeError(f'Test "{self.name}" does not have executable set, if you intent to use it through inheritance, make sure "Test" is the first derived class')
+                raise RuntimeError(
+                    f'Test "{self.name}" does not have executable set, if you intent to use it through inheritance, make sure "Test" is the first derived class')
             self.executable = self
         elif isinstance(self.executable, type):
             self.executable = self.executable()
@@ -55,21 +51,21 @@ class Test(Logging):
         self.fullname = f'{self.executable.makefile.fullname}.{self.name}'
         self.file = Path(self.file) if self.file else None
         self.workingDir = self.executable.build_path
-        self._log_basename = re.sub(r'[^\w_. -]', '_', self.name)
 
     def basename(self, args, expected_result):
         if len(self) <= 1:
-            return self._log_basename
+            return self.name
         else:
-            base = self._log_basename
+            base = self.name
             if len(args) > 0:
                 base += f'-{"-".join([str(a) for a in args])}'
             if expected_result != 0:
                 base += f'-{expected_result}'
             return base
-    
+
     def outs(self, args, expected_result):
         base = self.basename(args, expected_result)
+        base = re.sub(r'[^\w_. -]', '_', base)
         out = self.workingDir / f'{base}.stdout'
         err = self.workingDir / f'{base}.stderr'
         return out, err
@@ -79,14 +75,14 @@ class Test(Logging):
         out, err, rc = await self.executable.execute(*args, no_raise=True, cwd=self.workingDir)
         out_log, out_err = self.outs(args, expected_result)
         async with aiofiles.open(out_log, 'w') as outlog, \
-                   aiofiles.open(out_err, 'w') as errlog:
+                aiofiles.open(out_err, 'w') as errlog:
             async with asyncio.TaskGroup() as group:
                 group.create_task(outlog.write(out))
                 group.create_task(errlog.write(err))
         if rc != expected_result:
             out = out.strip()
             err = err.strip()
-            msg =  f'Test \'{self.name}\' failed (returned: {rc}, expected: {expected_result}) !'
+            msg = f'Test \'{self.name}\' failed (returned: {rc}, expected: {expected_result}) !'
             if out:
                 msg += '\nstdout: ' + out
             if err:
@@ -97,12 +93,13 @@ class Test(Logging):
         try:
             async with asyncio.TaskGroup() as tests:
                 for args, expected_result in self.cases:
-                    tests.create_task(self._run_test(args, expected_result=expected_result))
+                    tests.create_task(self._run_test(
+                        args, expected_result=expected_result))
         except asyncio.ExceptionGroup as errors:
             for err in errors.errors:
                 self.error(err)
             return False
         return True
-    
+
     def __len__(self):
         return len(self.cases)
