594503b016355e89ee6a391f572cf6dc9b1c228d
Sylvain Garcia <garcia.6l20@gmail.com>
2023-05-17 08:45:34 +0200

    add mbedtls example

--- examples/cxx/dan.io/dan-build.py
+++ examples/cxx/dan.io/dan-build.py
@@ -1,6 +1,7 @@
+import hashlib
 from dan import requires
 from dan.cxx import Executable
-from dan.testing import Test
+from dan.testing import Test, Case
 
 catch2, = requires('catch2 = 3')
 
@@ -15,9 +16,29 @@ class UseCatch2(Executable):
 
 class TestSpdlog(Test, Executable):
     name = 'test-spdlog'
-    sources= 'test_spdlog.cpp',
+    sources = 'test_spdlog.cpp',
     private_includes= '.',
     dependencies = (
         'spdlog = 1',
         'fmt = 9',
     )
+
+
+class TestMbedTLS(Test, Executable):
+    name = 'test-mbedtls'
+    sources = 'test_mbedtls.cpp',
+    dependencies = (
+        'mbedtls:mbedcrypto = 3',
+        'fmt = 9',
+    )
+
+    def make_expected_result(case: Case):
+        sha = hashlib.sha256()
+        for arg in case.args:
+            sha.update(arg.encode())
+        return f'SHA-256: {sha.hexdigest()}'
+
+    cases = [
+        Case('hello', 'hello', expected_output=make_expected_result),
+        Case('hello-dan', 'hello', 'dan', expected_output=make_expected_result),
+    ]
---
+++ examples/cxx/dan.io/test_mbedtls.cpp
@@ -0,0 +1,27 @@
+#include <mbedtls/sha256.h>
+
+#include <fmt/printf.h>
+
+#include <string_view>
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fmt::print("Expecting at least one argument");
+    }
+    mbedtls_sha256_context ctx;
+    mbedtls_sha256_init(&ctx);
+    mbedtls_sha256_starts(&ctx, 0);
+    for (int ii = 1; ii < argc; ++ii) {
+        auto a = std::string_view{argv[ii]};
+        mbedtls_sha256_update(&ctx, reinterpret_cast<const unsigned char*>(a.data()), a.size());
+    }
+    unsigned char result[32] = "\0";
+    mbedtls_sha256_finish(&ctx, result);
+    mbedtls_sha256_free(&ctx);
+    std::string sha256;
+    for (auto const item: result) {
+        fmt::format_to(std::back_inserter(sha256), "{:02x}", item);
+    }
+    fmt::print("SHA-256: {}\n", sha256);
+    return 0;
+}
