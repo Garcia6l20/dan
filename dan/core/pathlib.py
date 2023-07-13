c8d10350ba05e449db313735f08a686c9e6a9276
Sylvain Garcia <garcia.6l20@gmail.com>
2023-07-13 11:10:56 +0200

    pathlib: dont use inheritance

--- dan/core/pathlib.py
+++ dan/core/pathlib.py
@@ -1,33 +1,46 @@
 import os
 import pathlib
 
-class Path(type(pathlib.Path())):
+Path = type(pathlib.Path())
 
-    @property
-    def modification_time(self):
-        return self.stat().st_mtime
+@property
+def modification_time(self):
+    return self.stat().st_mtime
 
-    def younger_than(self, other):
-        time = other if isinstance(other, float) else other.modification_time
-        assert isinstance(time, float)
-        return self.modification_time > time
+Path.modification_time = modification_time
 
-    def older_than(self, other):
-        time = other if isinstance(other, float) else other.modification_time
-        assert isinstance(time, float)
-        return self.modification_time < time
 
-    def utime(self, *args, **kw_args):
-        os.utime(self, *args, **kw_args)
+def younger_than(self, other):
+    time = other if isinstance(other, float) else other.modification_time
+    assert isinstance(time, float)
+    return self.modification_time > time
 
-    @property
-    def is_empty(self):
-        if os.path.exists(self) and not os.path.isfile(self):
-    
-            # Checking if the directory is empty or not
-            if not os.listdir(self):
-                return True
-            else:
-                return False
+Path.younger_than = younger_than
+
+
+def older_than(self, other):
+    time = other if isinstance(other, float) else other.modification_time
+    assert isinstance(time, float)
+    return self.modification_time < time
+
+Path.older_than = older_than
+
+
+def utime(self, *args, **kw_args):
+    os.utime(self, *args, **kw_args)
+
+Path.utime = utime
+
+@property
+def is_empty(self):
+    if os.path.exists(self) and not os.path.isfile(self):
+
+        # Checking if the directory is empty or not
+        if not os.listdir(self):
+            return True
         else:
             return False
+    else:
+        return False
+
+Path.is_empty = is_empty
