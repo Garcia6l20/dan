2f2b59545200f0fdb5fab185deb856e0bc76002a
Sylvain Garcia <garcia.6l20@gmail.com>
2023-07-24 06:21:25 +0200

    fix qt.moc

--- examples/cxx/qt/dan-build.py
+++ examples/cxx/qt/dan-build.py
@@ -1,9 +1,8 @@
-from dan import self
 from dan.cxx import Executable
 from dan.cxx.support import qt
 from dan.pkgconfig import has_package
 
-if has_package('Qt5Core'):
+if has_package('Qt6Core'):
     @qt.moc(modules = ['Widgets'])
     class QtExample(Executable):
         name = 'dan-qt-example'
