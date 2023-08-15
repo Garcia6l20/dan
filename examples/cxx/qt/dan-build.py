2b0b919d2d8e873602294d0c8cace7209d947d5d
Sylvain Garcia <garcia.6l20@gmail.com>
2023-08-15 14:33:49 +0200

    qt: ui and recources

--- examples/cxx/qt/dan-build.py
+++ examples/cxx/qt/dan-build.py
@@ -3,7 +3,7 @@ from dan.cxx.support import qt
 from dan.pkgconfig import has_package
 
 if has_package('Qt6Core'):
-    @qt.moc(modules = ['Widgets'])
+    @qt.wrap(modules = ['Widgets'])
     class QtExample(Executable):
         name = 'dan-qt-example'
         sources = 'main.cpp', 'mainwindow.cpp'
