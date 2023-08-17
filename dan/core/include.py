dcb7020d0cc7dade7f250a1e61d5d4c10daf5f5e
Garcia Sylvain <garcia.6l20@gmail.com>
2023-08-17 07:14:24 +0200

    fix include strategy
     - after a makefile get included we shall not go back to parent but to previous
     - contexts might be activable recursively

--- dan/core/include.py
+++ dan/core/include.py
@@ -1,4 +1,5 @@
 import importlib.util
+from contextlib import contextmanager
 import os
 import sys
 
@@ -35,10 +36,10 @@ def requires(*requirements) -> list[Target]:
 class Context(Logging):
     def __init__(self) -> None:
         self.__root: MakeFile = None
-        self.__current: MakeFile = None
         self.__all_makefiles: set[MakeFile] = set()
         self.imported_makefiles: dict[Path, MakeFile] = dict()
-        self.__prev_ctx: Context = None
+        self.__ctx_stack: list[Context] = []
+        self.__makefile_stack: list[MakeFile] = []
         self.__attributes = dict()
         super().__init__('context')
 
@@ -48,26 +49,12 @@ class Context(Logging):
 
     @property
     def current(self):
-        return self.__current
+        return self.__makefile_stack[-1] if self.__makefile_stack else None
 
     @property
     def all_makefiles(self) -> set[MakeFile]:
         return self.__all_makefiles
 
-    @current.setter
-    def current(self, current: MakeFile):
-        if self.__root is None:
-            self.__root = current
-            self.__current = current
-        else:
-            current.parent = self.__current
-            self.__current = current
-
-        self.__all_makefiles.add(current)
-
-    def up(self):
-        if self.__current != self.__root:
-            self.__current = self.__current.parent
 
     def get(self, name, default=None):
         if name in self.__attributes:
@@ -81,14 +68,40 @@ class Context(Logging):
 
     def __enter__(self):
         global context
-        self.__prev_ctx = context
+        self.__ctx_stack.append(context)
         context = self
         return self
 
     def __exit__(self, *exc):
         global context
-        context = self.__prev_ctx
-        self.__prev_ctx = None
+        context = self.__ctx_stack.pop()
+        assert context is not None
+
+    @contextmanager
+    def _init_makefile(self, module, name: str = 'root', build_path: Path = None, requirements: MakeFile = None, parent: MakeFile = None, is_requirement=False):
+        source_path = Path(module.__file__).parent
+
+        if self.__root is None:
+            self.__root = module
+
+        if parent is None and self.current is not None:
+            parent = self.current
+
+        if build_path is None:
+            assert parent is not None
+            build_path = build_path or parent.build_path / name
+
+        module.__class__ = MakeFile
+        self.__makefile_stack.append(module)
+        module._setup(
+            name,
+            source_path,
+            build_path,
+            requirements,
+            parent,
+            is_requirement)
+        yield module
+        self.__makefile_stack.pop()
 
 
 context: Context = Context()
@@ -98,26 +111,6 @@ class MakeFileError(RuntimeError):
         self.path = Path(path)
         super().__init__(f'failed to load {self.path}')
 
-
-def _init_makefile(module, name: str = 'root', build_path: Path = None, requirements: MakeFile = None, parent: MakeFile = None, is_requirement=False):
-    global context
-    source_path = Path(module.__file__).parent
-    if parent is None and context.current is not None:
-        parent = context.current
-
-    if build_path is None:
-        assert parent is not None
-        build_path = build_path or parent.build_path / name
-
-    module.__class__ = MakeFile
-    context.current = module
-    module._setup(
-        name,
-        source_path,
-        build_path,
-        requirements,
-        parent,
-        is_requirement)
 
 def load_makefile(module_path: Path,
                   name: str = None,
@@ -134,13 +127,12 @@ def load_makefile(module_path: Path,
         module_name, module_path)
     module = importlib.util.module_from_spec(spec)
     context.imported_makefiles[module_path] = module
-    _init_makefile(module, name, build_path, requirements, parent, is_requirement)
-    try:
-        spec.loader.exec_module(module)
-    except Exception as err:
-        context.error('makefile error while loading \'%s\': %s', module_path, err)
-        raise MakeFileError(module_path) from err
-    context.up()
+    with context._init_makefile(module, name, build_path, requirements, parent, is_requirement):
+        try:
+            spec.loader.exec_module(module)
+        except Exception as err:
+            context.error('makefile error while loading \'%s\': %s', module_path, err)
+            raise MakeFileError(module_path) from err
     return module
 
 
@@ -180,22 +172,21 @@ def include_makefile(name: str | Path, build_path: Path = None) -> set[Target]:
 
     module = importlib.util.module_from_spec(spec)
     context.imported_makefiles[module_path] = module
-    _init_makefile(module, name, build_path)
 
-    requirements_file = module_path.with_stem('dan-requires')
-    if module_path.stem == 'dan-build' and requirements_file.exists():
-        context.current.requirements = load_makefile(
-            requirements_file, name='dan-requires', module_name=f'{name}.requirements', is_requirement=True)
+    with context._init_makefile(module, name, build_path):
+        requirements_file = module_path.with_stem('dan-requires')
+        if module_path.stem == 'dan-build' and requirements_file.exists():
+            context.current.requirements = load_makefile(
+                requirements_file, name='dan-requires', module_name=f'{name}.requirements', is_requirement=True)
 
-    try:
-        spec.loader.exec_module(module)
-    except TargetNotFound as err:
-        if len(context.missing) == 0:
-            raise err
-    except Exception as err:
-        context.error('makefile error while including \'%s\': %s', module_path, err)
-        raise MakeFileError(module_path) from err
-    context.up()
+        try:
+            spec.loader.exec_module(module)
+        except TargetNotFound as err:
+            if len(context.missing) == 0:
+                raise err
+        except Exception as err:
+            context.error('makefile error while including \'%s\': %s', module_path, err)
+            raise MakeFileError(module_path) from err
 
 
 def include(*names: str | Path) -> list[Target]:
